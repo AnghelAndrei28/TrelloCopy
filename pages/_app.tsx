08619420c4a1c3b3dfcb752edaea252a862a72ae
Anghel Andrei <andrei.anghel2810@gmail.com>
2024-12-05 05:03:06 +0200

    Fix posthog error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import posthog from "posthog-js"
 import { PostHogProvider } from 'posthog-js/react'
+import { v4 as uuidv4 } from 'uuid';
 
 if (typeof window !== 'undefined') { // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
-    },
-  })
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com';
+
+  if (posthogKey) {
+    posthog.init(posthogKey, {
+      api_host: posthogHost,
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+      },
+    });
+
+    const id = uuidv4();
+    posthog.identify(id, { id });
+  } else {
+    console.error('PostHog key is not defined');
+  }
 }
 
 export default function App({ Component, pageProps }: AppProps) {
